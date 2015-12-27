94306f85ac3f995a0a1f80dac3891d32f45e368d
Viktor Pishchulin <i@viktorp.com>
2015-12-26 20:17:05 -0500

    Changed mongodb credentials.

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,15 @@
+var connection_string = 'mongodb://localhost:27017/mamahelp';
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 module.exports = {
-    db: process.env.MONGODB || 'mongodb://localhost:27017/mamahelp',
+    db: connection_string,
 
     cryptos: {
         algorithm: 'aes256',
