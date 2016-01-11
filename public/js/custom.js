0f69469c62975a69a41182ef5b1c276fce02805e
yulia.pi <yulia.pi@gmail.com>
2016-01-11 10:50:20 -0500

    Site search dropdown transition fixed.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,46 +1,31 @@
 /**
  * Created by yulia on 1/8/2016.
  */
-/***Search expand****/
+/***Site Search expand-collapse****/
 (function (window) {
     var input = $('#nav-search-input');
-    var label = $('#nav-search-label');
-    console.log('label', label);
+    var label = $('.nav-search-label');
     label.click(function () {
         input.addClass('search-expanded');
         label.addClass('search-active');
         $('#search-terms').focus();
-        setUnsensitive(input);
     });
     document.addEventListener('click', function (e) {
-        //var clickedId = e.target.id;
-        var clickedClass = e.target.className;
-        console.log('clicked class: ' + clickedClass);
-        if (clickedClass.indexOf('js-click-unsensitive') === -1) {
+        console.log('event target: ' + $(e.target));
+        console.log('closest parent: ' + $(e.target).closest('#nav-search-input').length);
+        if ($(e.target).closest('#nav-search-input').length === 0
+            && $(e.target).closest('#site-search-button').length === 0) {
             $('#nav-search-input').removeClass('search-expanded');
-            $('#nav-search-label').removeClass('search-active');
+            $('.nav-search-label').removeClass('search-active');
         }
+    });
 
-        /*if (clickedId != 'search-label'
-            && clickedId != 'nav-search-label'
-         && clickedId != 'nav-search-input'
-         && clickedId != 'search-terms'
-         && clickedId != 'glyphicon-search1'
-         && clickedId != 'glyphicon-search2'
-         && clickedId != 'site-search'
-         && clickedClass != 'search-input-wrapper') {
-         console.log('Closing search active.');
-         $('#nav-search-input').removeClass('search-expanded');
-         $('#nav-search-label').removeClass('search-active');
-         }*/
-
-    })
-
+    $('.glyphicon-remove').click(function () {
+        $('#nav-search-input').removeClass('search-expanded');
+        $('.nav-search-label').removeClass('search-active');
+    });
 }(window));
 
-function setUnsensitive(element) {
-    console.log('setting class unsencitive to element: ' + element);
-    element.find('*').addClass('js-click-unsensitive');
-    element.addClass('js-click-unsensitive');
-}
+
+
 
